933bd8c8896b338af728b1bd6905078368d39a71
Kshitij Verma <kshitijv250@gmail.com>
2024-04-07 09:56:45 +0530

    added chat feature

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -3,18 +3,25 @@ import AllChat from "./components/allChat";
 import Window from "./components/window";
 import { MessageSquareDiffIcon } from "lucide-react";
 
-export default function Chat(to:any){
-    const user=useAuthProvider();
+export default function Chat() {
+  const to = "1";
+  const user = useAuthProvider();
+  console.log("user", user);
 
-    return(
-        <div className="w-full flex justify-center items-center h-screen">
-            <div className="max-w-2xl flex w-full h-screen bg-gray-100 dark:border-gray-600 dark:bg-gray-900 border-r-2 flex-col">
-                <div className="w-full border-b-2 flex p-5 dark:border-gray-600 justify-between items-center"><span>All Chats</span><button className="rounded-full hover:bg-gray-200 p-2"><MessageSquareDiffIcon/></button></div>
-                <AllChat/>
-            </div>
-            <div className="w-full flex h-screen bg-gray-100 dark:bg-gray-900">
-                <Window to={to.value} from={user.user?.id}/>
-            </div>
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex h-screen w-full max-w-2xl flex-col border-r-2 bg-gray-100 dark:border-gray-600 dark:bg-gray-900">
+        <div className="flex w-full items-center justify-between border-b-2 p-5 dark:border-gray-600">
+          <span>All Chats</span>
+          <button className="rounded-full p-2 hover:bg-gray-200">
+            <MessageSquareDiffIcon />
+          </button>
         </div>
-    );
+        <AllChat />
+      </div>
+      <div className="flex h-screen w-full bg-gray-100 dark:bg-gray-900">
+        <Window to={to} from={user.user?.id ?? ""} message={""} />
+      </div>
+    </div>
+  );
 }
